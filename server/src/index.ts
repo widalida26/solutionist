fc45946ace8d0ec41469dca049035670b1511607
Wi <widalida26@gmail.com>
2021-12-30 01:53:58 +0900

    Fix : Connect DB with server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,17 @@
 import express from 'express';
-import { createConnection } from 'typeorm';
+import { createConnection, Connection } from 'typeorm';
+import { users } from './entity/user';
 
 const app = express();
+
+createConnection({
+  type: 'mysql',
+  database: `${process.env.RDS_HOST}`,
+  username: `${process.env.RDS_USERNAME}`,
+  password: `${process.env.RDS_PASSWORD}`,
+  host: `${process.env.RDS_HOST}`,
+  entities: [users],
+});
 
 app.get('/', (req, res) => {
   res.send('hello');
