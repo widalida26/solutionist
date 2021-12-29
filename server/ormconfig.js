7c35f8abfe02b7bcaf6801c7d4aecdceae23bf90
Wi <widalida26@gmail.com>
2021-12-29 20:07:46 +0900

    Update : Setting for migration

---
+++ server/ormconfig.js
@@ -0,0 +1,18 @@
+module.exports = {
+  type: 'mysql',
+  host: process.env.RDS_HOST,
+  port: process.env.RDS_PORT,
+  username: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DATABASE,
+  synchronize: true,
+  logging: true,
+  entities: ['src/entity/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+};
