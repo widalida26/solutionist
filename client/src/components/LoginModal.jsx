f51b7427984db3009c7c442d1a573636beac46fc
JAM-PARK <chwk05@gmail.com>
2021-12-30 21:23:31 +0900

    Update : Loginmodal CSS

--- client/src/components/LoginModal.jsx
+++ client/src/components/LoginModal.jsx
@@ -6,18 +6,46 @@ import '../App.css';
 const LoginModal = () => {
   return (
     <>
-      <div className="div-flex">
-        <span>Solutionist</span>
+      <div className="flex">
+        <div className="flex flex_dir_col wid150 hei600 bg_col flex_center">
+          <span>Solutionist</span>
+        </div>
+        <div className="flex flex_dir_col wid300 hei432 pad_5rem_2rem border mar_le10 flex_around">
+          <div className="flex flex_dir_col info_box">
+            <label>이메일</label>
+            <input></input>
+            <label>비밀번호</label>
+            <input></input>
+          </div>
+          <div className="flex flex_between">
+            <div className="flex gap1">
+              <button>G</button>
+              <button>K</button>
+            </div>
+            <button>로그인</button>
+          </div>
+          <span className="flex flex_end">아직 계정이 없으신가요?</span>
+        </div>
       </div>
-      <div className="div-flex">
-        <label>이메일</label>
-        <input></input>
-        <label>비밀번호</label>
-        <input></input>
-        <button>G</button>
-        <button>K</button>
-        <button>로그인</button>
-        <span>아직 계정이 없으신가요?</span>
+      <br />
+      <br />
+      <div className="flex">
+        <div className="flex flex_dir_col wid300 hei432 pad_5rem_2rem border mar_ri10 flex_around">
+          <label>이메일</label>
+          <input></input>
+          <label>닉네임</label>
+          <input></input>
+          <label>비밀번호</label>
+          <input></input>
+          <label>비밀번호 확인</label>
+          <input></input>
+          <div className="flex flex_end">
+            <button>회원가입</button>
+          </div>
+        </div>
+        <div className="flex flex_dir_col wid150 hei600 bg_col flex_center">
+          <span>Solutionist</span>
+        </div>
       </div>
     </>
   );
