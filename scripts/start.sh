3fb0e1a0da47417f0859fdec065e6e987983d742
Wi <widalida26@gmail.com>
2021-12-30 20:50:49 +0900

    Fis : Command in start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/solutionist/server
-authbind --deep ./node_modules/.bin/pm2 start src/index.ts --watch
+authbind --deep ./node_modules/.bin/pm2 start dist/src/index.js --watch
