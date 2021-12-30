ccdb6b41c1f65639fd88fe4ee6797392e00763fa
JAM-PARK <chwk05@gmail.com>
2021-12-30 15:07:48 +0900

    Update : AWS test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,16 @@ import Nav from './components/Nav.jsx';
 
 const App = () => {
   return (
-    <div>
-      <Nav></Nav>
-      <p>Hello, dev aws!</p>
-      <p>Hello, AWS!!!!!</p>
-    </div>
+    <>
+      <div>
+        <Nav></Nav>
+        <p>Hello, dev aws!</p>
+        <p>Hello, AWS!!!!!</p>
+      </div>
+      <div>
+        <p style={{ color: 'red' }}>final test</p>
+      </div>
+    </>
   );
 };
 
