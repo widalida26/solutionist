b863be457c44ef05075febe21019149b8a9dbf3b
JLthePi <jlthepi@gmail.com>
2021-12-31 13:38:52 +0900

    Add : Drop down CSS

--- client/src/components/MenuDropDown.jsx
+++ client/src/components/MenuDropDown.jsx
@@ -2,17 +2,19 @@ import * as React from 'react';
 
 const MenuDropDown = () => {
   return (
-    <div className="w_180 bg_main absolute menu_drop_down flex_col">
-      <div className="flex w_full">
+    <div className="menu_drop_down w_180 absolute flex_col">
+      <div className="flex w_full mar_b_10">
         <div className="bg_dark w_40 h_40 circle"></div>
         <div className="flex_1 h_full mar_l_10">
           <div className="font_14 mar_b_5">김코딩</div>
           <div className="font_10">kimcoding@gmail.com</div>
         </div>
       </div>
-      <div className="">나의 세트</div>
-      <div className="">설정</div>
-      <div className="">로그아웃</div>
+      <ul className="menu_list w_full flex_col">
+        <li className="menu">나의 세트</li>
+        <li className="menu">프로필 설정</li>
+        <li className="menu">로그아웃</li>
+      </ul>
     </div>
   );
 };
