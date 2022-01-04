2ab03764018eb26c30d32c157534356a75f525b6
Wi <widalida26@gmail.com>
2022-01-05 07:12:22 +0900

    Move : Interface folder to src

--- server/src/service/sets.ts
+++ server/src/service/sets.ts
@@ -1,6 +1,24 @@
+import { Repository } from 'typeorm';
+import { Service } from 'typedi';
 import { sets } from '../database/entity/sets';
-import { ISetsDTO } from 'interface/ISets';
+import { ISetsDTO } from '../interface/ISets';
 
-export default class SetsService {
-  async setsMaker(set: ISetsDTO) {}
+@Service()
+export class SetService {
+  constructor(private setsRepo: Repository<sets>) {
+    this.setsRepo = setsRepo;
+  }
+
+  // 세트 정보 삽입
+  async setMaker(userId: number, set: ISetsDTO) {
+    await this.setsRepo.save({
+      userId,
+      ...set,
+    });
+  }
+
+  // 세트 정보 삭제
+  async setRemover(title: string): Promise<void> {
+    await this.setsRepo.delete({ title: title });
+  }
 }
