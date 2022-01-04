aed966d44e8acd6819bb35cbc959d7a95412b748
Wi <widalida26@gmail.com>
2022-01-05 07:11:50 +0900

    Add : Set service

--- server/src/controllers/sets/add.ts
+++ server/src/controllers/sets/add.ts
@@ -1,18 +1,21 @@
 import { Request, Response } from 'express';
-import { ISetsDTO, IProblems, IChoices } from '../../../interface/ISets';
+import { ISetsDTO, IProblems, IChoices } from '../../interface/ISets';
 import errorGenerator from '../../error/errorGenerator';
 
 const add = (req: Request, res: Response) => {
   const set: ISetsDTO = req.body;
   const problems: IProblems[] = set['problems'];
-  // const choices: IChoices[] = problems.map((problem) => {
-  //   return {problem['choices']};
-  // });
 
   // 누락된 데이터가 있을 경우 에러 리턴
-  // if (!set || !problems || !choices) {
-  //   errorGenerator({ statusCode: 400 });
-  // }
+  if (!set || !problems) {
+    errorGenerator({ statusCode: 400 });
+  }
+
+  let userId: number = 1;
+
+  // const choices: IChoices[] = problems.map((problem) => {
+  //   return {problem['choices']};c
+  // });
 
   res.send();
 };
