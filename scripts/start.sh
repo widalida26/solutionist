f227f6fa970e66978bdd8ded9aef987b772f6819
Wi <widalida26@gmail.com>
2022-01-04 13:23:40 +0900

    Fix : Commands in start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/solutionist/server
-authbind --deep ./node_modules/.bin/pm2 start dist/src/index.js --watch
+authbind --deep pm2 start npm -- start
+#authbind --deep ./node_modules/.bin/pm2 start dist/src/index.js --watch
