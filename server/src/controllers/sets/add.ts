3a0e18f6ffc649cbd4c8e7743289370308a6e665
Wi <widalida26@gmail.com>
2022-01-04 16:15:21 +0900

    Add : Middleware setting

---
+++ server/src/controllers/sets/add.ts
@@ -0,0 +1,7 @@
+import express, { Request, Response } from 'express';
+import { addListener } from 'process';
+
+const add = async (req: Request, res: Response) => {
+  res.send();
+};
+export default add;
