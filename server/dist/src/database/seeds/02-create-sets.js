9ca27e7b45003be4f3a48377024a739f49967f8e
Wi <widalida26@gmail.com>
2022-01-04 15:01:25 +0900

    Fix : Source of ormconfig

--- server/dist/src/database/seeds/02-create-sets.js
+++ server/dist/src/database/seeds/02-create-sets.js
@@ -1,21 +1,10 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const sets_1 = require("../entity/sets");
-const sets_2 = require("../../../dummy/sets");
-class CreateInitialUserData {
-    run(factory, connection) {
-        return __awaiter(this, void 0, void 0, function* () {
-            yield connection.createQueryBuilder().insert().into(sets_1.sets).values(sets_2.dummySets).execute();
-        });
-    }
-}
-exports.CreateInitialUserData = CreateInitialUserData;
+// import { Connection } from 'typeorm';
+// import { Factory, Seeder } from 'typeorm-seeding';
+// import { sets } from '../entity/sets';
+// import { dummySets } from '../../../dummy/sets';
+// export class CreateInitialUserData implements Seeder {
+//   public async run(factory: Factory, connection: Connection): Promise<any> {
+//     await connection.createQueryBuilder().insert().into(sets).values(dummySets).execute();
+//   }
+// }
 //# sourceMappingURL=02-create-sets.js.map
--- server/dist/src/database/seeds/03-create-problems.js
+++ server/dist/src/database/seeds/03-create-problems.js
@@ -1,26 +1,15 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const problems_1 = require("../entity/problems");
-const problems_2 = require("../../../dummy/problems");
-class CreateInitialUserData {
-    run(factory, connection) {
-        return __awaiter(this, void 0, void 0, function* () {
-            yield connection
-                .createQueryBuilder()
-                .insert()
-                .into(problems_1.problems)
-                .values(problems_2.dummyProblems)
-                .execute();
-        });
-    }
-}
-exports.CreateInitialUserData = CreateInitialUserData;
+// import { Connection } from 'typeorm';
+// import { Factory, Seeder } from 'typeorm-seeding';
+// import { problems } from '../entity/problems';
+// import { dummyProblems } from '../../../dummy/problems';
+// export class CreateInitialUserData implements Seeder {
+//   public async run(factory: Factory, connection: Connection): Promise<any> {
+//     await connection
+//       .createQueryBuilder()
+//       .insert()
+//       .into(problems)
+//       .values(dummyProblems)
+//       .execute();
+//   }
+// }
 //# sourceMappingURL=03-create-problems.js.map
