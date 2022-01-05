ceb742dd50003d75ee6360e0396b170429150309
Wi <widalida26@gmail.com>
2022-01-05 19:55:39 +0900

    Fix : Merge conflict

--- server/src/routes/sets.ts
+++ server/src/routes/sets.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import add from '../controllers/sets/add';
 import asyncfy from 'express-asyncify';
 import search from '../controllers/sets/search';
-import { Token } from '../middleware/checkauth';
+import checktoken from '../middleware/checkauth';
 
 const setsRouter = asyncfy(express.Router());
 
 setsRouter.get('/sets', search); // 세트 검색
-setsRouter.post('/choices', Token, add); // 세트 제작
+setsRouter.post('/choices', checktoken, add); // 세트 제작
 
 export default setsRouter;
