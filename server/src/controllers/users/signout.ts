b917717975de18a10281cf9351c815492f2655cf
inde153 <inde153@naver.com>
2022-01-06 02:13:43 +0900

    Add : signout

--- server/src/controllers/users/signout.ts
+++ server/src/controllers/users/signout.ts
@@ -1,13 +1,29 @@
 import { Request, Response } from 'express';
-import cryptos from '../../crypto';
 import { users } from '../../database/entity/users';
 import errorGenerator from '../../error/errorGenerator';
-import { getRepository } from 'typeorm';
-import jwtToken from '../tokenFunctions/index';
+import { getRepository, getConnection } from 'typeorm';
 
 const signout = async (req: Request, res: Response) => {
   try {
-    console.log('hi signout');
+    const { id, email } = res.locals.userInfo;
+
+    const removeUser = await getConnection()
+      .createQueryBuilder()
+      .delete()
+      .from(users)
+      .where('id = :id OR email = :email', { id: id, email: email })
+      .execute();
+
+    // createConnection().then(async (connection) => {
+    //   console.log(555);
+    //   const removeUser = await connection.manager.findOne(users, {
+    //     where: { id: id, email: email },
+    //   });
+    //   console.log(444);
+    //   await connection.manager.remove(removeUser);
+    // }).catch(error => console.log(error));
+
+    return res.status(200).send('successfully signed out');
   } catch (err) {
     console.log(err);
     return res.status(500).send('internal server error');
