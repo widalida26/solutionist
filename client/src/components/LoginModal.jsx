37c775190846399d9c1443436ef943830741e1e4
JAM-PARK <chwk05@gmail.com>
2022-01-06 00:06:22 +0900

    Add : Google oauth setting

--- client/src/components/LoginModal.jsx
+++ client/src/components/LoginModal.jsx
@@ -403,9 +403,12 @@ const LoginModal = ({ isLoginModalOn, onModalOffAction, isLogin, onloginAction }
     window.location.assign(
       `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&state=google`
     );
+
+    // TODO : 리디렉션 완료 => code 떼오기 => useEffect로 api요청 => 서버로 code 보내기
+    // ! redirect_uri 확인하기
   };
 
-  // * 임시 코드 : 새로고침 모달 켜기
+  // * 구글 Oauth 리디렉션 코드 post로 보내기
   // useEffect(() => {});
 
   return (
@@ -448,21 +451,13 @@ const LoginModal = ({ isLoginModalOn, onModalOffAction, isLogin, onloginAction }
                       <ButtonContainer>
                         <ButtonGroup>
                           <IconBorder>
-                            <FcGoogle />
+                            <FcGoogle onClick={handleSignGoogle} />
                           </IconBorder>
                           <IconBorder>
                             <RiKakaoTalkFill />
                           </IconBorder>
                         </ButtonGroup>
                         <StyledButton onClick={handleLogin}>LOGIN</StyledButton>
-                        <StyledButton
-                          onClick={() => {
-                            onModalOffAction();
-                            onloginAction();
-                          }}
-                        >
-                          FAKE
-                        </StyledButton>
                       </ButtonContainer>
                       {errorMessage ? <div>{errorMessage}</div> : ''}
                       {afterSignUp ? (
