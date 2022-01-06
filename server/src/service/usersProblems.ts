c2901dffa2475127dde8475b62f2b25a9f961199
Wi <widalida26@gmail.com>
2022-01-06 16:06:57 +0900

    Add : uuid

--- server/src/service/usersProblems.ts
+++ server/src/service/usersProblems.ts
@@ -2,29 +2,41 @@ import errorGenerator from '../error/errorGenerator';
 import { Service } from 'typedi';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 import { uProblemsRepository } from '../database/repository/usersProblems';
+import { ProblemsRepository } from '../database/repository/problems';
 import { ISetsDTO, IProblems, IChoices, ISolveDTO } from '../interface/ISets';
+import { solve } from 'src/controllers/sets';
+import { problems } from 'src/database/entity/problems';
 
 @Service()
 export class uProblemsService {
-  constructor(@InjectRepository() private upRepo: uProblemsRepository) {}
+  constructor(
+    @InjectRepository() private upRepo: uProblemsRepository,
+    @InjectRepository() private problemsRepo: ProblemsRepository
+  ) {}
 
-  async uProblemsMaker(solveInfo: ISolveDTO, userId?: number): Promise<void> {
+  async uProblemsMaker(solveInfo: ISolveDTO, email?: string): Promise<void> {
     // 필요한 정보가 누락된 경우
     if (!solveInfo.problemId || !solveInfo.choice) {
       errorGenerator({ statusCode: 400 });
     }
 
-    if (!userId) {
+    if (!email) {
       // 로그인된 유저가 아닌 경우 임의의 id 설정 => 음수 id 생성
-      const minInt = -214748364;
-      userId = Math.floor(Math.random() * (0 - minInt) + minInt);
+      //const minInt = -214748364;
+      //email = Math.floor(Math.random() * (0 - minInt) + minInt);
     }
 
-    this.upRepo.save({
-      userId,
+    const foundProblem = await this.problemsRepo.findOne({ id: solveInfo.problemId });
+    // problems 테이블에 problemId에 해당하는 레코드가 없는 경우
+    if (!foundProblem) {
+      errorGenerator({ statusCode: 400 });
+    }
+
+    await this.upRepo.save({
+      email,
       ...solveInfo,
     });
 
-    await this.upRepo.save({});
+    //await this.upRepo.save({});
   }
 }
