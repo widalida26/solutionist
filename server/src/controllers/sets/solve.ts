bb1a38fad86be4768512edce99d59948b2eb4b15
Wi <widalida26@gmail.com>
2022-01-06 15:31:04 +0900

    Add : usersProblems maker

--- server/src/controllers/sets/solve.ts
+++ server/src/controllers/sets/solve.ts
@@ -3,33 +3,25 @@ import { Request, Response } from 'express';
 import errorGenerator from '../../error/errorGenerator';
 import { ISetsDTO, ISolveDTO } from '../../interface/ISets';
 import { IUsersDTO } from '../../interface/IUsers';
-import { SetService } from '../../service/sets';
+import { uProblemsService } from '../../service/usersProblems';
 import { solvedSets } from '../../database/entity/solvedSets';
 import { emptyObjectCk } from '../../utils/custom';
+import { usersProblems } from 'src/database/entity/usersProblems';
 
 const solve = async (req: Request, res: Response) => {
   // 토큰 인증에 실패했을 경우 = 유저 정보가 없을 경우 => 빈 객체 할당
   const userInfo: IUsersDTO = res.locals.userInfo ? res.locals.userInfo : {};
-
-  // 로그인된 유저가 아닌 경우 임의의 id 설정 => 음수 id 생성
-  const minInt = -214748364;
-  if (emptyObjectCk(userInfo)) {
-    userInfo.id = Math.floor(Math.random() * (0 - minInt) + minInt);
-  }
 
   const solveDTO: ISolveDTO = req.body;
   // 데이터가 누락됐을 경우
   if (emptyObjectCk(solveDTO)) {
     errorGenerator({ statusCode: 400 });
   }
-  console.log('solve', solveDTO);
-  //   // 쿼리 값이 부적합할 경우
-  //   if (!searchWord) {
-  //     errorGenerator({ statusCode: 400 });
-  //   }
 
-  // sets 테이블 이용을 위한 setService 인스턴스
-  const setServiceInstance: SetService = Container.get(SetService);
+  // usersProblems 테이블 이용을 위한 usersProblems 인스턴스
+  const upServiceInstance: uProblemsService = Container.get(uProblemsService);
+
+  //upServiceInstance.uProblemsMaker(solveDTO);
 
   res.end();
 };
--- server/src/service/usersProblems.ts
+++ server/src/service/usersProblems.ts
@@ -2,16 +2,28 @@ import errorGenerator from '../error/errorGenerator';
 import { Service } from 'typedi';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 import { uProblemsRepository } from '../database/repository/usersProblems';
-import { ISetsDTO, IProblems, IChoices } from '../interface/ISets';
+import { ISetsDTO, IProblems, IChoices, ISolveDTO } from '../interface/ISets';
 
 @Service()
-export class SetService {
+export class uProblemsService {
   constructor(@InjectRepository() private upRepo: uProblemsRepository) {}
 
-  async uProblemsMaker(userId: number, problemId: number, choice: number): Promise<void> {
-    // 로그인된 유저가 아닐 경우
-    if (userId === -1) {
+  async uProblemsMaker(solveInfo: ISolveDTO, userId?: number): Promise<void> {
+    // 필요한 정보가 누락된 경우
+    if (!solveInfo.problemId || !solveInfo.choice) {
+      errorGenerator({ statusCode: 400 });
     }
+
+    if (!userId) {
+      // 로그인된 유저가 아닌 경우 임의의 id 설정 => 음수 id 생성
+      const minInt = -214748364;
+      userId = Math.floor(Math.random() * (0 - minInt) + minInt);
+    }
+
+    this.upRepo.save({
+      userId,
+      ...solveInfo,
+    });
 
     await this.upRepo.save({});
   }
