99c02994ace0ad8b57f829f42cdbd9489390d6cd
Wi <widalida26@gmail.com>
2022-01-06 11:45:50 +0900

    Fix : API doc

---
+++ server/src/controllers/sets/index.ts
@@ -0,0 +1,5 @@
+import search from './search';
+import add from './add';
+import solve from './solve';
+
+export { search, add, solve };
--- server/src/routes/sets.ts
+++ server/src/routes/sets.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import add from '../controllers/sets/add';
+import { search, add, solve } from '../controllers/sets/index';
 import asyncfy from 'express-asyncify';
-import search from '../controllers/sets/search';
 import { saveUserInfo } from '../middleware/checkauth';
 
 const setsRouter = asyncfy(express.Router());
 
 setsRouter.get('/sets', search); // 세트 검색
 setsRouter.post('/choices', saveUserInfo, add); // 세트 제작
+setsRouter.post('/usersProblems', saveUserInfo, solve); // 문제 풀기
 
 export default setsRouter;
