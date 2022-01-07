3292cd4b76eee0093bdf82e8d927e99dfb090ec2
Wi <widalida26@gmail.com>
2022-01-07 17:18:45 +0900

    Rename : add controller to create

--- server/src/controllers/sets/index.ts
+++ server/src/controllers/sets/index.ts
@@ -1,5 +1,5 @@
 import search from './search';
-import add from './add';
+import create from './create';
 import solve from './solve';
 
-export { search, add, solve };
+export { search, create, solve };
--- server/src/routes/sets.ts
+++ server/src/routes/sets.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import { search, add, solve } from '../controllers/sets/index';
+import { search, create, solve } from '../controllers/sets/index';
 import asyncfy from 'express-asyncify';
 import { saveUserInfo } from '../middleware/checkauth';
 
 const setsRouter = asyncfy(express.Router());
 
 setsRouter.get('/sets', search); // 세트 검색
-setsRouter.post('/sets', saveUserInfo, add); // 세트 제작
+setsRouter.post('/sets', saveUserInfo, create); // 세트 제작
 setsRouter.post('/usersProblems', saveUserInfo, solve); // 문제 풀기
 
 export default setsRouter;
