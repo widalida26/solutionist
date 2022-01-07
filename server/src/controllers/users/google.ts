0487f63cdb25bec651de32ad68161baffab17015
inde153 <inde153@naver.com>
2022-01-07 12:11:35 +0900

    Fix : google login

--- server/src/controllers/users/google.ts
+++ server/src/controllers/users/google.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 import jwtToken from '../../utils/tokenFunctions/index';
 
 const google = async (req: Request, res: Response) => {
-  const googletokenURL = 'https://oauth2.googleapis.com/token';
+  // const googletokenURL = 'https://oauth2.googleapis.com/token';
   const googleInfoURL = 'https://www.googleapis.com/oauth2/v2/userinfo';
   const googleClientId = process.env.GOOGLE_CLIENT_ID;
   const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
@@ -14,24 +14,38 @@ const google = async (req: Request, res: Response) => {
     const tokenRes = await axios.post(
       `https://oauth2.googleapis.com/token?code=${req.body.authorizationCode}&client_id=${googleClientId}&client_secret=${googleClientSecret}&redirect_uri=${process.env.CLIENT_URI}&grant_type=authorization_code`
     );
-    const accessToken = tokenRes.data.access_token;
 
-    console.log(555, accessToken);
+    const { access_token: accessToken } = tokenRes.data;
     const userInfo = await axios.get(googleInfoURL, {
       headers: {
         authorization: `Bearer ${accessToken}`,
       },
     });
+
     const { name: username, email, picture: profileImage } = userInfo.data;
+    console.log(999, username, profileImage);
     const info = getRepository(users);
     const findUser = await info.findOne({ where: { email: email } });
+
     if (!findUser) {
       const user = await getConnection()
         .createQueryBuilder()
         .insert()
         .into(users)
-        .values([{ username: username, email: email, type: 'google' }])
+        .values([{ username: username, email: email, type: 'google', profileImage }])
         .execute();
+
+      const secondFind = await info.findOne({ where: { email: email } });
+      const playload = {
+        id: secondFind.id,
+        username: secondFind.username,
+        email: secondFind.email,
+        type: secondFind.type,
+      };
+      const accessToken = jwtToken.accessToken(playload);
+      jwtToken.sendAccessToken(res, accessToken);
+      console.log('accessToken', accessToken);
+      return res.status(201).json({ data: playload });
     } else {
       const playload = {
         id: findUser.id,
@@ -39,14 +53,12 @@ const google = async (req: Request, res: Response) => {
         email: findUser.email,
         type: findUser.type,
       };
-      const id = findUser.id;
       const accessToken = jwtToken.accessToken(playload);
-      console.log(111, playload);
       jwtToken.sendAccessToken(res, accessToken);
-      return res.status(201).json({ id, username, email, profileImage });
+      console.log('accessToken', accessToken);
+      return res.status(201).json({ data: playload });
     }
   } catch (error) {
-    console.log(error);
     return res.status(500).send('Internal Server Error');
   }
 };
