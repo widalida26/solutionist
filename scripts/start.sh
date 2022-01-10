5a013d1580a9ec61712d37bf2bf1431996187496
Wi <widalida26@gmail.com>
2022-01-10 14:23:26 +0900

    Add : console

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,7 @@ authbind --deep tsc
 
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
 export SECRET_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names SECRET_KEY --query Parameters[0].Value | sed 's/"//g')
+console.log(SECRET_KEY)
 
 cd /home/ubuntu/solutionist/server/dist/src
 authbind --deep pm2 start -f index.js
