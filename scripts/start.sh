c00b41ebf5ace5ab56a2e1fe5ee28cc38524a0c7
Wi <widalida26@gmail.com>
2022-01-10 14:31:39 +0900

    Fix : echo

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ authbind --deep tsc
 
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
 export SECRET_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names SECRET_KEY --query Parameters[0].Value | sed 's/"//g')
-echo 'secret key':$SECRET_KEY
+echo $(aws ssm get-parameters --region ap-northeast-2 --names SECRET_KEY --query Parameters[0].Value | sed 's/"//g')
 
 cd /home/ubuntu/solutionist/server/dist/src
 authbind --deep pm2 start -f index.js
