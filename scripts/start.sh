e096ebd1d36a49513c124a0be086e897758412c6
Wi <widalida26@gmail.com>
2022-01-10 15:16:51 +0900

    Fix : Parameter store

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,9 +4,13 @@ authbind --deep tsc
 
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
 export SECRET_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names SECRET_KEY --query Parameters[0].Value | sed 's/"//g')
-echo $(aws ssm get-parameters --region ap-northeast-2 --names SECRET_KEY --query Parameters[0].Value | sed 's/"//g')
-echo $(SECRET_KEY)
+export GOOGLE_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_CLIENT_ID --query Parameters[0].Value | sed 's/"//g')
+export GOOGLE_CLIENT_SECRET
+=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_CLIENT_SECRET
+ --query Parameters[0].Value | sed 's/"//g')
+ export CLIENT_URI
+=$(aws ssm get-parameters --region ap-northeast-2 --names CLIENT_URI
+ --query Parameters[0].Value | sed 's/"//g')
 
-
-cd /home/ubuntu/solutionist/server/dist/src
+cd /home/ubuntu/solutiosnist/server/dist/src
 authbind --deep pm2 start -f index.js
