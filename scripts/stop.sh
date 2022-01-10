eb3faacbf584e3a1fea01d130933e5ca0f267c21
Wi <widalida26@gmail.com>
2022-01-10 13:57:31 +0900

    Fix : stop path

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/im-sprint-practice-deploy/server
-pm2 stop index.ts 2> /dev/null || true
-pm2 delete index.ts 2> /dev/null || true
+cd /home/ubuntu/solutionist/server/dist/src
+pm2 stop index.js 2> /dev/null || true
+pm2 delete index.js 2> /dev/null || true
