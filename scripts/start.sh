aad33cec29b3846deb173d49e67bf6d521a5b44b
Wi <widalida26@gmail.com>
2022-01-10 14:27:13 +0900

    Remove : Echo

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,6 @@ authbind --deep tsc
 
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
 export SECRET_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names SECRET_KEY --query Parameters[0].Value | sed 's/"//g')
-echo SECRET_KEY
 
 cd /home/ubuntu/solutionist/server/dist/src
 authbind --deep pm2 start -f index.js
