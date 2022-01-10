45b17e11591dacfd2f9086203abe533d2c75b2a2
Wi <widalida26@gmail.com>
2022-01-10 11:27:41 +0900

    Fix : start.sh command

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/solutionist/server
 authbind --deep tsc
-cd /home/ubuntu/solutionist/server/dist/src
-authbind --deep pm2 start index.js
+cd /home/ubuntu/solutionist/server
+pm2 start npm -- start
+#authbind --deep pm2 start index.js
