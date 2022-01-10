21e478492689aeb2c5ef91377c8a5a9769f6294d
Wi <widalida26@gmail.com>
2022-01-10 13:53:05 +0900

    Add : f option

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,4 +6,4 @@ export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names AC
 export SECRET_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names SECRET_KEY --query Parameters[0].Value | sed 's/"//g')
 
 cd /home/ubuntu/solutionist/server/dist/src
-authbind --deep pm2 start index.js
+authbind --deep pm2 start -f index.js
