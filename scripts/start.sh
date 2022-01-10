2d2215320be66bf5d42ddf1012ba359a16e20cd4
Wi <widalida26@gmail.com>
2022-01-10 18:06:04 +0900

    Add : parameters

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,21 @@
 cd /home/ubuntu/solutionist/server
 authbind --deep tsc
 
+export RDS_USERNAME
+=$(aws ssm get-parameters --region ap-northeast-2 --names RDS_USERNAME
+ --query Parameters[0].Value | sed 's/"//g')
+export RDS_PASSWORD
+=$(aws ssm get-parameters --region ap-northeast-2 --names RDS_PASSWORD
+ --query Parameters[0].Value | sed 's/"//g')
+export RDS_PORT
+=$(aws ssm get-parameters --region ap-northeast-2 --names RDS_PORT
+ --query Parameters[0].Value | sed 's/"//g')
+export RDS_DATABASE
+=$(aws ssm get-parameters --region ap-northeast-2 --names RDS_DATABASE
+ --query Parameters[0].Value | sed 's/"//g')
+export RDS_HOST
+=$(aws ssm get-parameters --region ap-northeast-2 --names RDS_HOST
+ --query Parameters[0].Value | sed 's/"//g')
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
 export SECRET_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names SECRET_KEY --query Parameters[0].Value | sed 's/"//g')
 export GOOGLE_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_CLIENT_ID --query Parameters[0].Value | sed 's/"//g')
