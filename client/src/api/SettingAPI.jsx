05e6f7c3ba6f16fec01ff5d33786f8ebf713ae22
JAM-PARK <chwk05@gmail.com>
2022-01-11 14:46:30 +0900

    Fix : Profile api

--- client/src/api/SettingAPI.jsx
+++ client/src/api/SettingAPI.jsx
@@ -1,21 +1,23 @@
 const axios = require('axios');
 
 export function changeProfileImage(file) {
-  return axios
-    .patch(
-      `${process.env.SERVER_URL}users/profileImage`,
-      {
-        file,
-      },
-      {
+  // console.log('axios:', file);
+  if (file) {
+    const formData = new FormData();
+    formData.append('file', file);
+    // for (var value of formData.values()) {
+    //   console.log(value);
+    // }
+    return axios
+      .patch(`${process.env.SERVER_URL}myPage/profileImage`, formData, {
         headers: {
-          'Content-Type': `application/json`,
+          'Content-Type': 'multipart/form-data',
         },
         withCredentials: true,
-      }
-    )
-    .then((res) => {
-      console.log('changeProfileImage 요청 성공, res:', res);
-      return res;
-    });
+      })
+      .then((res) => {
+        console.log('changeProfileImage 요청 성공, res:', res);
+        return res;
+      });
+  }
 }
