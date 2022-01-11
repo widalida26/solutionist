1a7f34fb7817ffdd8585a145bea41093c3b8f240
JLthePi <jlthepi@gmail.com>
2022-01-12 02:13:35 +0900

    Update : Support responsive at Make

--- client/src/components/MakeProblem.jsx
+++ client/src/components/MakeProblem.jsx
@@ -14,53 +14,71 @@ import CheckBoldIcon from '../icons/CheckBold';
 
 const ProblemContainer = styled.div`
   display: grid;
-  grid-template-rows: 1fr;
-  grid-template-columns: 1fr 56.6% 1fr;
+  grid-template-rows: auto auto auto;
+  grid-template-columns: 25% 37.5% 12.5% 25%;
+  grid-template-areas:
+    'number question icons .'
+    'number choice choice .'
+    'number explanation explanation .';
+
+  @media all and (max-width: 1023px) {
+    grid-template-columns: 25% 45% 15% 15%;
+  }
+  @media all and (max-width: 767px) {
+    margin: 0 1rem;
+    grid-template-rows: auto auto auto auto;
+    grid-template-columns: 2fr 1fr 1fr;
+    grid-template-areas:
+      'number icons icons '
+      'question question question'
+      'choice choice choice '
+      'explanation explanation explanation ';
+  }
 `;
 const ProblemNum = styled.div`
+  grid-area: number;
   text-align: end;
-  margin-right: 1rem;
   color: var(--orangey-yellow);
   font-size: ${(props) => props.font_size};
   opacity: 0.5;
+  margin-right: 1rem;
+
+  @media all and (max-width: 767px) {
+    font-size: 2rem;
+    text-align: start;
+    margin-top: 1rem;
+  }
 `;
-const Problem = styled.div`
-  display: grid;
-  grid-template-rows: repeat(3, auto);
-  grid-template-columns: 3fr 1fr;
-  grid-template-areas:
-    'question icons'
-    'list list-count'
-    'explanation explanation';
-`;
-const ProblemOx = styled.div`
-  display: grid;
-  grid-template-rows: repeat(3, auto);
-  grid-template-columns: 3fr 1fr;
-  grid-template-areas:
-    'question icons'
-    'list list'
-    'explanation explanation';
-`;
+
 const Question = styled.textarea`
   grid-area: question;
-  height: 48px;
-  margin: 2rem 0 1rem 1rem;
+  height: 24px;
+  margin-top: 1rem;
   line-height: 120%;
   word-wrap: break-word;
   word-break: break-word;
-  font-size: 2.5rem;
+  font-size: 1.25rem;
   font-family: 'GongGothicMedium', sans-serif;
   resize: none;
+  @media all and (max-width: 767px) {
+    margin-top: 0.5rem;
+  }
 `;
 const IconContainer = styled.div`
   display: flex;
   justify-content: flex-end;
+  align-items: center;
   grid-area: icons;
-  margin-top: 2rem;
+  margin-top: 1rem;
 `;
 const Icon = styled.div`
-  margin: 0.5rem 1rem auto 0;
+  width: 1.75rem;
+  height: 1.75rem;
+  margin: 0 0.25rem;
+  svg {
+    width: 1.75rem;
+    height: 1.75rem;
+  }
   :hover {
     svg {
       fill: black;
@@ -72,42 +90,52 @@ const Icon = styled.div`
     }
   }
 `;
-const ListContainer = styled.ol`
-  grid-area: list;
-`;
-const List = styled.li`
+const ChoicesFlex = styled.div`
+  grid-area: choice;
   display: flex;
-  margin-top: 1rem;
-  margin-left: 1rem;
+  justify-content: space-between;
+`;
+const ChoicesContainer = styled.ol`
+  flex: 1;
+  margin-right: 1rem;
+  li {
+    margin-top: 0.25rem;
+  }
+  li:first-child {
+    margin-top: 0;
+  }
+`;
+const Choice = styled.li`
+  display: flex;
   align-items: center;
   border-bottom: 1px solid var(--warm-grey);
   color: var(--warm-grey);
 `;
-const ListNum = styled.div`
+const ChoiceNum = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 2.5rem;
-  height: 3.5rem;
   align-self: end;
-  font-size: 2rem;
+  width: 2rem;
+  margin-bottom: 0.25rem;
+  font-size: 1.25rem;
 `;
-const ListContent = styled.textarea`
+const ChoiceContent = styled.textarea`
   flex: 1;
   width: 100%;
-  height: 40px;
-  margin: 0.5rem 0 0.5rem 1rem;
+  height: 24px;
+  margin: 0.25rem 0.5rem 0.25rem 0;
   color: black;
-  font-size: 1.75rem;
+  font-size: 1rem;
   font-family: 'GowunDodum-Regular', sans-serif;
   word-wrap: break-word;
   word-break: break-word;
   resize: none;
 `;
-const ListCheck = styled.div`
+const CheckContainer = styled.div`
   display: flex;
-  width: 2rem;
-  height: 3.5rem;
+  height: 1.5rem;
+  width: 1.5rem;
   margin-right: 0.5rem;
   align-self: end;
   :hover {
@@ -116,21 +144,17 @@ const ListCheck = styled.div`
     }
   }
   svg {
-    margin-bottom: 0.5rem;
     align-self: end;
   }
 `;
-const ListCount = styled.div`
-  grid-area: list-count;
-`;
 const CounterContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 3.5rem;
-  margin-top: 1rem;
+  height: 70px;
   color: var(--warm-grey);
-  font-size: 2rem;
+  font-size: 1.25rem;
 `;
 const Counter = styled.div`
   width: auto;
@@ -138,20 +162,17 @@ const Counter = styled.div`
 `;
 const ExplanationContainer = styled.div`
   grid-area: explanation;
-  margin-top: 2.5rem;
-  padding-bottom: 2.5rem;
-  border-bottom: 2px solid var(--orangey-yellow);
 `;
 const Explanation = styled.textarea`
-  height: 67px;
-  width: calc(100% - 6rem - 2px);
-  margin: 0 1rem;
-  padding: 1rem 2rem;
-  border: 1px solid var(--warm-grey);
+  height: 33px;
+  width: calc(100% - 1.5rem - 2px);
+  margin: 1rem 0;
+  padding: 0.5rem 0.75rem;
+  border: 1px dashed var(--warm-grey);
   border-radius: 10px;
   background-color: white;
   color: var(--warm-grey);
-  font-size: 1.5rem;
+  font-size: 0.75rem;
   font-family: 'GowunDodum-Regular', sans-serif;
   word-wrap: break-word;
   word-break: break-word;
@@ -159,34 +180,43 @@ const Explanation = styled.textarea`
 `;
 const OxContainer = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-evenly;
+  grid-area: choice;
   margin-top: 1rem;
 `;
 const OxCard = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 12rem;
-  height: 12rem;
-  margin: 0 4.2%;
-  padding: 8%;
+  width: 35%;
+  height: 100%;
+  max-width: 12rem;
+  max-height: 12rem;
   background-color: white;
   box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.16);
   border-radius: 10px;
   svg {
     height: 100%;
     width: 100%;
+    margin: 2rem;
     :hover {
       fill: var(--orangey-yellow);
     }
   }
+  @media all and (max-width: 767px) {
+    max-width: 10rem;
+    max-height: 10rem;
+    svg {
+      margin: 1.5rem;
+    }
+  }
 `;
 const CountController = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
+  height: 1.5rem;
+  width: 1.5rem;
+  svg {
+    height: 1.5rem;
+  }
 `;
 
 const MakeProblem = ({ problem, data, setData, idx, navRefs }) => {
@@ -250,11 +280,11 @@ const MakeProblem = ({ problem, data, setData, idx, navRefs }) => {
 
   return (
     <ProblemContainer ref={(el) => (navRefs.current[idx] = el)}>
-      <ProblemNum font_size={idx + 1 > 99 ? '8rem' : '12rem'}>
+      <ProblemNum font_size={idx + 1 > 99 ? '6rem' : '8rem'}>
         <p>{idx + 1}</p>
       </ProblemNum>
       {problem.isOx ? (
-        <ProblemOx>
+        <>
           <Question
             placeholder="문제를 입력해주세요."
             onInput={autoGrow}
@@ -276,26 +306,20 @@ const MakeProblem = ({ problem, data, setData, idx, navRefs }) => {
               <TrashIcon fill="var(--warm-grey)" />
             </Icon>
           </IconContainer>
-          <ListContainer>
-            <OxContainer>
-              <OxCard onClick={handleClick} id="O">
-                <OIcon
-                  id="O"
-                  fill={
-                    problem.answer === 1 ? 'var(--orangey-yellow)' : 'var(--warm-grey)'
-                  }
-                />
-              </OxCard>
-              <OxCard onClick={handleClick} id="X">
-                <XIcon
-                  id="X"
-                  fill={
-                    problem.answer === 2 ? 'var(--orangey-yellow)' : 'var(--warm-grey)'
-                  }
-                />
-              </OxCard>
-            </OxContainer>
-          </ListContainer>
+          <OxContainer>
+            <OxCard onClick={handleClick} id="O">
+              <OIcon
+                id="O"
+                fill={problem.answer === 1 ? 'var(--orangey-yellow)' : 'var(--warm-grey)'}
+              />
+            </OxCard>
+            <OxCard onClick={handleClick} id="X">
+              <XIcon
+                id="X"
+                fill={problem.answer === 2 ? 'var(--orangey-yellow)' : 'var(--warm-grey)'}
+              />
+            </OxCard>
+          </OxContainer>
           <ExplanationContainer>
             <Explanation
               placeholder="해설"
@@ -304,9 +328,9 @@ const MakeProblem = ({ problem, data, setData, idx, navRefs }) => {
               id="explanation"
             />
           </ExplanationContainer>
-        </ProblemOx>
+        </>
       ) : (
-        <Problem>
+        <>
           <Question
             placeholder="문제를 입력해주세요."
             onInput={autoGrow}
@@ -328,31 +352,31 @@ const MakeProblem = ({ problem, data, setData, idx, navRefs }) => {
               <TrashIcon fill="var(--warm-grey)" />
             </Icon>
           </IconContainer>
-          <ListContainer>
-            {problem.choices.map((choice, idx) => (
-              <List key={`choice ${idx + 1}`}>
-                <ListNum>{`${idx + 1}.`}</ListNum>
-                <ListContent
-                  placeholder={`${idx + 1}번 보기`}
-                  onChange={handleChange}
-                  value={choice.content}
-                  id={`c${idx}`}
-                  onInput={autoGrow}
-                />
-                <ListCheck onClick={handleClick} id={`a${idx}`}>
-                  <CheckIcon
-                    idx={`${idx}`}
-                    fill={
-                      choice.index === problem.answer
-                        ? 'var(--vibrant-green)'
-                        : 'var(--warm-grey)'
-                    }
+          <ChoicesFlex>
+            <ChoicesContainer>
+              {problem.choices.map((choice, idx) => (
+                <Choice key={`choice ${idx + 1}`}>
+                  <ChoiceNum>{`${idx + 1}.`}</ChoiceNum>
+                  <ChoiceContent
+                    placeholder={`${idx + 1}번 보기`}
+                    onChange={handleChange}
+                    value={choice.content}
+                    id={`c${idx}`}
+                    onInput={autoGrow}
                   />
-                </ListCheck>
-              </List>
-            ))}
-          </ListContainer>
-          <ListCount>
+                  <CheckContainer onClick={handleClick} id={`a${idx}`}>
+                    <CheckIcon
+                      idx={`${idx}`}
+                      fill={
+                        choice.index === problem.answer
+                          ? 'var(--vibrant-green)'
+                          : 'var(--warm-grey)'
+                      }
+                    />
+                  </CheckContainer>
+                </Choice>
+              ))}
+            </ChoicesContainer>
             <CounterContainer>
               <CountController onClick={handleClick} id="decrease">
                 <DecreaseIcon id="decrease" fill="var(--warm-grey)" />
@@ -362,7 +386,7 @@ const MakeProblem = ({ problem, data, setData, idx, navRefs }) => {
                 <IncreaseIcon id="increase" fill="var(--warm-grey)" />
               </CountController>
             </CounterContainer>
-          </ListCount>
+          </ChoicesFlex>
           <ExplanationContainer>
             <Explanation
               placeholder="해설"
@@ -372,7 +396,7 @@ const MakeProblem = ({ problem, data, setData, idx, navRefs }) => {
               onInput={autoGrow}
             />
           </ExplanationContainer>
-        </Problem>
+        </>
       )}
     </ProblemContainer>
   );
