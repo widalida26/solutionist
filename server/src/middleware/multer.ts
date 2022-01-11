98497b1688547f2b552ab825dfa184859ed898b8
inde153 <inde153@naver.com>
2022-01-11 11:42:57 +0900

    Fix : meddleware

--- server/src/middleware/multer.ts
+++ server/src/middleware/multer.ts
@@ -2,7 +2,6 @@ import multer from 'multer';
 import multerS3 from 'multer-s3';
 import aws from 'aws-sdk';
 import 'dotenv/config';
-import path from 'path';
 
 const s3 = new aws.S3({
   accessKeyId: process.env.BUCKET_KEY_ID,
