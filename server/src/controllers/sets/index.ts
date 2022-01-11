8b1cf2d40cd5977988ad0327588ee32dffa04169
Wi <widalida26@gmail.com>
2022-01-11 20:54:25 +0900

    Add : Routing for select

--- server/src/controllers/sets/index.ts
+++ server/src/controllers/sets/index.ts
@@ -1,7 +1,8 @@
 import search from './search';
+import select from './select';
 import create from './create';
 import modify from './modify';
 import solve from './solve';
 import submit from './submit';
 
-export { search, create, modify, solve, submit };
+export { search, select, create, modify, solve, submit };
--- server/src/routes/sets.ts
+++ server/src/routes/sets.ts
@@ -1,14 +1,15 @@
 import express from 'express';
-import { search, create, modify, solve, last } from '../controllers/sets/index';
+import { search, select, create, modify, solve, submit } from '../controllers/sets/index';
 import asyncfy from 'express-asyncify';
 import { saveUserInfo, blockUnauthorized } from '../middleware/checkauth';
 
 const setsRouter = asyncfy(express.Router());
 
 setsRouter.get('/sets', search); // 세트 검색
+setsRouter.get('/sets/:setsId', select); // 세트 선택
 setsRouter.post('/collections', saveUserInfo, create); // 세트 제작
 setsRouter.post('/sets', blockUnauthorized, modify); // 세트 수정
 setsRouter.post('/solveSets', saveUserInfo, solve); // 문제 풀기
-setsRouter.post('/solvedSets', saveUserInfo, last); // 마지막 문제 풀기
+setsRouter.post('/solvedSets', saveUserInfo, submit); // 세트 제출
 
 export default setsRouter;
