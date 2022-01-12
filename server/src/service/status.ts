11c1d9a23f397bf150f127867591d5fcacb8c318
Wi <widalida26@gmail.com>
2022-01-12 22:13:57 +0900

    Add : Verify status record

--- server/src/service/status.ts
+++ server/src/service/status.ts
@@ -12,7 +12,7 @@ import { emptyObjectValueCk } from '../utils/custom';
 export class StatusService {
   constructor(
     @InjectRepository() private statusRepo: solveStatusRepository,
-    @InjectRepository() private recrodRepo: SolveRecordsRepository,
+    @InjectRepository() private recordRepo: SolveRecordsRepository,
     @InjectRepository() private problemsRepo: ProblemsRepository,
     @InjectRepository() private choicesRepo: ChoicesRepository
   ) {}
@@ -23,23 +23,8 @@ export class StatusService {
       errorGenerator({ statusCode: 400 });
     }
 
-    // problems 테이블에 problemId가 있는지 조회
-    const setId = await this.problemsRepo.findOne(solveInfo.problemId).then((result) => {
-      // problems 테이블에 problemId에 해당하는 레코드가 없는 경우
-      if (!result) {
-        errorGenerator({ statusCode: 400 });
-      } else {
-        return result.setId;
-      }
-    });
-
-    // solveRecords 테이블에 recrodId가 있는지 조회
-    await this.recrodRepo.findOne({ id: solveInfo.recordId, setId }).then((result) => {
-      // solveRecords 테이블에 recrodId가 해당하는 레코드가 없는 경우
-      if (!result) {
-        errorGenerator({ statusCode: 400 });
-      }
-    });
+    // 삽입할 데이터 검증
+    await this.verifyStatusToSave(solveInfo.recordId, solveInfo.problemId);
 
     // choice가 유효한지 확인 => 0 이하거나 가장 마지막 index보다 크면 안됨
     const maxIdx = await this.choicesRepo.getLastChoice();
@@ -61,6 +46,34 @@ export class StatusService {
       id,
       ...selectionRate,
     };
+  }
+
+  async verifyStatusToSave(recordId: number, problemId: number) {
+    this.problemsRepo.createQueryBuilder('problems');
+    // problems 테이블에 problemId가 있는지 조회
+    const setId = await this.problemsRepo.findOne(problemId).then((result) => {
+      // problems 테이블에 problemId에 해당하는 레코드가 없는 경우
+      if (!result) {
+        errorGenerator({ statusCode: 400 });
+      } else {
+        return result.setId;
+      }
+    });
+
+    // solveRecords 테이블에 recrodId가 있는지 조회
+    await this.recordRepo.findOne({ id: recordId, setId }).then((result) => {
+      // solveRecords 테이블에 recrodId가 해당하는 레코드가 없는 경우
+      if (!result) {
+        errorGenerator({ statusCode: 400 });
+      }
+    });
+
+    // 같은 recordId와 problemId를 가진 데이터는 삽입할 수 없음
+    await this.statusRepo.checkDuplicate(recordId, problemId).then((result) => {
+      if (result) {
+        errorGenerator({ statusCode: 400 });
+      }
+    });
   }
 
   async selectionRateCalculator(maxIdx: number, problemId: number) {
