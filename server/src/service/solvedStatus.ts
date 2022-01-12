0de17df939af854931d82be3352706abca4d872c
Wi <widalida26@gmail.com>
2022-01-12 13:38:26 +0900

    Update : Problem solver

--- server/src/service/solvedStatus.ts
+++ server/src/service/solvedStatus.ts
@@ -2,33 +2,44 @@ import errorGenerator from '../error/errorGenerator';
 import { Service } from 'typedi';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 import { solveStatusRepository } from '../database/repository/solveStatus';
+import { SolvedRecordsRepository } from '../database/repository/solveRecords';
 import { ProblemsRepository } from '../database/repository/problems';
 import { ChoicesRepository } from '../database/repository/choices';
 import { ISolve } from '../interface/ISets';
-import { v4 } from 'uuid';
+import { emptyObjectValueCk } from 'src/utils/custom';
 
 @Service()
 export class StatusService {
   constructor(
     @InjectRepository() private statusRepo: solveStatusRepository,
+    @InjectRepository() private recrodRepo: SolvedRecordsRepository,
     @InjectRepository() private problemsRepo: ProblemsRepository,
     @InjectRepository() private choicesRepo: ChoicesRepository
   ) {}
 
   async ProblemSolver(solveInfo: ISolve) {
     // 필요한 정보가 누락된 경우
-    if (!solveInfo.solver || !solveInfo.problemId || !solveInfo.choice) {
+    if (emptyObjectValueCk(solveInfo)) {
       errorGenerator({ statusCode: 400 });
     }
 
     // problems 테이블에 problemId가 있는지 조회
-    const foundProblem = await this.problemsRepo.findOne({
-      id: solveInfo.problemId,
+    const setId = await this.problemsRepo.findOne(solveInfo.problemId).then((result) => {
+      // problems 테이블에 problemId에 해당하는 레코드가 없는 경우
+      if (!result) {
+        errorGenerator({ statusCode: 400 });
+      } else {
+        return result.setId;
+      }
     });
-    // problems 테이블에 problemId에 해당하는 레코드가 없는 경우
-    if (!foundProblem) {
-      errorGenerator({ statusCode: 400 });
-    }
+
+    // solveRecords 테이블에 recrodId가 있는지 조회
+    await this.recrodRepo.findOne({ id: solveInfo.recordId, setId }).then((result) => {
+      // solveRecords 테이블에 recrodId가 해당하는 레코드가 없는 경우
+      if (!result) {
+        errorGenerator({ statusCode: 400 });
+      }
+    });
 
     // choice가 유효한지 확인 => 0 이하거나 가장 마지막 index보다 크면 안됨
     const maxIdx = await this.choicesRepo.getLastChoice();
