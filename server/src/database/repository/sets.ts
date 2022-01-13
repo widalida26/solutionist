fb9d8326058af7ec88136e536535eb79bdb6b53e
Wi <widalida26@gmail.com>
2022-01-13 14:15:32 +0900

    Remove : Unnecessary commens

--- server/src/database/repository/sets.ts
+++ server/src/database/repository/sets.ts
@@ -20,26 +20,6 @@ export class SetsRepository extends Repository<sets> {
       .getRawMany();
     console.log(dt);
   }
-  // await this.createQueryBuilder('sets');
-  // collection 아이디로 groupby한 다음 가장 최신의 set를 가져와야 함
-  // return await this.createQueryBuilder('sets')
-  //   .leftJoin('sets.creator', 'users')
-  //   .select([
-  //     'sets.id',
-
-  //     'users.username',
-  //   ])
-  //   .where('sets.title like :title', { title: '%' + title + '%' })
-  //   .getMany()
-  //   .then((result) => {
-  //     console.log('result', result);
-  //     return result.map((el) => {
-  //       //console.log(el);
-  //       //el['username'] = el.creator ? el.creator.username : null;
-  //       //delete el.creator;
-  //       //return el;
-  //     });
-  //   });
 
   // setId로 세트 검색
   async findSet(id: number) {
@@ -64,15 +44,4 @@ export class SetsRepository extends Repository<sets> {
         else return convertRawObject(result)['createdAt'];
       });
   }
-  //삭제된 세트의 userId 반환
-  // async getRemovedUser(id: number) {
-  //   return await this.findOne(id).then(async (set) => {
-  //     await this.delete(id);
-  //     if (!set) {
-  //       return null;
-  //     } else {
-  //       return set.creatorId;
-  //     }
-  //   });
-  // }
 }
