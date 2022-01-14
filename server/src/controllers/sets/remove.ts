667aacf999b14797db16ff3bb9f84e0075163a98
Wi <widalida26@gmail.com>
2022-01-15 02:44:00 +0900

    Implement : Collection delete

--- server/src/controllers/sets/remove.ts
+++ server/src/controllers/sets/remove.ts
@@ -1,12 +1,10 @@
 import { Request, Response } from 'express';
 import Container from 'typedi';
-
-import { SetService } from '../../service/sets';
+import { CollectionService } from '../../service/collections';
 import errorGenerator from '../../error/errorGenerator';
-import { checkEmptyObject } from '../../utils/custom';
 
 const remove = async (req: Request, res: Response) => {
-  const collectionId = req.query['collectionId'];
+  const collectionId = Number(req.params['collectionId']);
 
   // 쿼리 값이 부적합할 경우
   if (!collectionId) {
@@ -14,10 +12,10 @@ const remove = async (req: Request, res: Response) => {
   }
 
   // sets 테이블 이용을 위한 setService 인스턴스
-  const setServiceInstance: SetService = Container.get(SetService);
-  // collection
-  setServiceInstance;
+  const collectionServiceInstance: CollectionService = Container.get(CollectionService);
+  // collection 삭제
+  await collectionServiceInstance.removeCollection(collectionId);
 
-  res.status(201).json({ id: collectionId });
+  res.status(200).json({ id: collectionId });
 };
 export default remove;
--- server/src/service/collections.ts
+++ server/src/service/collections.ts
@@ -1,58 +1,12 @@
-import errorGenerator from '../error/errorGenerator';
-import { MoreThan } from 'typeorm';
 import { Service } from 'typedi';
 import { InjectRepository } from 'typeorm-typedi-extensions';
-import { SolveRecordsRepository } from '../database/repository/solveRecords';
-import { SetsRepository } from '../database/repository/sets';
+import { CollectionsRepository } from '../database/repository/collections';
 
 @Service()
-export class CollectionsService {
-  constructor(
-    @InjectRepository() private recordRepo: SolveRecordsRepository,
-    @InjectRepository() private setsRepo: SetsRepository
-  ) {}
+export class CollectionService {
+  constructor(@InjectRepository() private collectionRepo: CollectionsRepository) {}
 
-  async makeRecord(setId: number, userId: number): Promise<number> {
-    // 해당하는 세트가 없는 경우
-    await this.setsRepo.findOne(setId).then((result) => {
-      if (!result) {
-        errorGenerator({ statusCode: 400 });
-      }
-    });
-    // solveRecords 테이블에 삽입
-    const recordId = await this.recordRepo
-      .save({ setId, userId })
-      .then((result) => (result ? result.id : null));
-    // solvedRecords 삽입에 실패한 경우
-    if (!recordId) {
-      errorGenerator({ statusCode: 500 });
-    }
-    return recordId;
-  }
-
-  async submitRecord(recordId: number, answerRate: number) {
-    // answerRate가 유효하지 않을 경우
-    if (answerRate < 0 || answerRate > 100) {
-      errorGenerator({ statusCode: 400 });
-    }
-
-    // id에 해당하는 레코드 있는지 확인
-    await this.recordRepo.save({ id: recordId, answerRate: answerRate });
-
-    return recordId;
-  }
-
-  // 해당 세트를 푼 유저를 카운트
-  async countRecord(setId: number) {
-    return await this.recordRepo.count({
-      where: {
-        setId,
-        answerRate: MoreThan(-1),
-      },
-    });
-  }
-
-  async getTotalAnswerRate(recordId: number) {
-    return await this.recordRepo.getAvgAnswerRate(recordId);
+  async removeCollection(collectionId: number) {
+    await this.collectionRepo.delete({ id: collectionId });
   }
 }
