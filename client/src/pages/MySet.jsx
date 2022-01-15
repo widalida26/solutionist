f5a8830de5f0254820d80a7b8c4a577139c67766
JAM-PARK <chwk05@gmail.com>
2022-01-16 01:20:20 +0900

    Update : Myset page structure

--- client/src/pages/MySet.jsx
+++ client/src/pages/MySet.jsx
@@ -12,15 +12,14 @@ const SetsContainer = styled.div`
   grid-template-areas:
     '. header .'
     '. cards .';
-  margin-top: 60px;
-  margin-bottom: 2rem;
-  grid-row-gap: 2rem;
+  margin: 1.2rem 0;
+  grid-row-gap: 1.2rem;
   grid-column-gap: 1rem;
 `;
 
 const Header = styled.div`
   grid-area: header;
-  font-size: 3.75rem;
+  font-size: 1.75rem;
 `;
 
 const CardsContainer = styled.div`
@@ -28,6 +27,17 @@ const CardsContainer = styled.div`
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-gap: 1rem;
   grid-area: cards;
+
+  div:nth-child(n + 5) {
+    display: none;
+  }
+`;
+
+const ShowBox = styled.section`
+  display: flex;
+  justify-content: flex-end;
+  width: 56.6%;
+  margin: 0 21.7% 1.2rem 21.7%;
 `;
 
 const Divider = styled.div`
@@ -45,27 +55,38 @@ const MySet = () => {
         <MoveTopButton />
         <Header>내가 만든 세트</Header>
         <CardsContainer>
-          <SetCardVerTwo></SetCardVerTwo>
-          <SetCard></SetCard>
-          <SetCard></SetCard>
-          <SetCard></SetCard>
-          <SetCard></SetCard>
-          <SetCard></SetCard>
-          <SetCard></SetCard>
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
         </CardsContainer>
       </SetsContainer>
+      <ShowBox>
+        <p>Show More</p>
+      </ShowBox>
       <Divider />
       <SetsContainer>
         <Header>내가 푼 세트</Header>
         <CardsContainer>
-          <SetCard></SetCard>
-          <SetCard></SetCard>
-          <SetCard></SetCard>
-          <SetCard></SetCard>
-          <SetCard></SetCard>
-          <SetCard></SetCard>
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
+          <SetCardVerTwo />
         </CardsContainer>
       </SetsContainer>
+      <ShowBox>
+        <p>Show More</p>
+      </ShowBox>
     </>
   );
 };
