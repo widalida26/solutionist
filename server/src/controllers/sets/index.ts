f792a0d7d971e0d8acdf0c04c9e3ad9384f42e4c
Wi <widalida26@gmail.com>
2022-01-17 22:10:08 +0900

    Add : Routing for popular

--- server/src/controllers/sets/index.ts
+++ server/src/controllers/sets/index.ts
@@ -2,24 +2,24 @@ import create from './create';
 import modify from './modify';
 import remove from './remove';
 import search from './search';
+import popular from './popular';
 import select from './select';
 import record from './record';
 import solve from './solve';
 import submit from './submit';
 import statics from './statics';
 import version from './version';
-import popular from './popular';
 
 export {
   create,
   modify,
   remove,
   search,
+  popular,
   select,
   record,
   solve,
   submit,
   statics,
   version,
-  popular,
 };
--- server/src/controllers/sets/popular.ts
+++ server/src/controllers/sets/popular.ts
@@ -4,19 +4,15 @@ import { SetService } from '../../service/sets';
 import errorGenerator from '../../error/errorGenerator';
 
 const popular = async (req: Request, res: Response) => {
-  const collectionId = Number(req.params['collectionId']);
-
-  // collection id가 유효하지 않을 경우
-  if (!collectionId) {
-    errorGenerator({ msg: 'empty or invalid collection id', statusCode: 400 });
-  }
+  console.log('인기');
 
   // sets 테이블 이용을 위한 setService 인스턴스
   const setServiceInstance: SetService = Container.get(SetService);
 
   // 세트 버전 조회
-  const versions = await setServiceInstance.findVersion(collectionId);
+  //const versions = await setServiceInstance.findVersion(collectionId);
 
-  res.status(200).json(versions);
+  //res.status(200).json(versions);
+  res.end();
 };
 export default popular;
